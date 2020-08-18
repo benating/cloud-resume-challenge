b543ccf5435348c55e25589467c524473cf9620e
Bernard Ting <ting.bsk97@gmail.com>
2020-08-18 18:49:32 +0100

    fix: change content-type of index.html to text/html

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -36,10 +36,11 @@ resource "aws_s3_bucket" "cv-bucket" {
 }
 
 resource "aws_s3_bucket_object" "cv-upload" {
-  bucket = aws_s3_bucket.cv-bucket.id
-  key    = "index.html"
-  source = "../../src/resources/CV.html"
-  etag   = filemd5("../../src/resources/CV.html")
+  bucket       = aws_s3_bucket.cv-bucket.id
+  key          = "index.html"
+  source       = "../../src/main/resources/CV.html"
+  content_type = "text/html"
+  etag         = filemd5("../../src/main/resources/CV.html")
 }
 
 resource "aws_s3_bucket_policy" "cv-bucket-policy" {
